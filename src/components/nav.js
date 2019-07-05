f770d70d45515827a56f0d4a083e5e28f8d37254
Andrew Sleigh <andrewsleigh@akauk.com>
2019-07-05 19:50:43 +0100

    drop down react js implemented setstate toggles this then adds to class name in truthy and falsey

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,79 +1,109 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, {Component} from "react"
 
 import "./nav.css"
 
-const Nav = () => (
-  <nav
-    style={{
-      background: `white`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <ul
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        fontFamily: `B612 Mono, monospace`,
-        fontSize: `0.6875rem`,
-      }}
-    >
-      <li className="nav-has-children" style={{ 
-            margin: `0 0 1.32rem`, 
-            listStyle: `none`,
-            color: `#222222`,
-            fontWeight: `bold`,
-        
-        }}>
-        Portfolios
+class Nav extends Component{
+  constructor(){
+    super();
 
-        <ul className="list-unstyled">
-            <li>Screen</li>
-            <li>Theatres</li>
-            <li>Commercial</li>
-            <li>Music</li>
-            <li>Street</li>
-            <li>Events</li>
-            <li>Projects</li>
-            <li>Clients</li>
-        </ul>
-      </li>
-      <li style={{ 
-          margin: 0,
-          listStyle: `none`,
-          margin: `0 0 1.32rem`,
-          fontWeight: `bold`,
-         }}>
-        <Link
-          to="/"
+    this.state = {
+      collapsed: true,
+    }
+
+    this.showMenu = this.showMenu.bind(this)
+  }
+
+  showMenu(event){
+    event.preventDefault();
+
+    this.setState({
+      collapsed: !this.state.collapsed,
+    })
+  }
+  render(){
+    const classDropdownMenu = 'navbar-collapse' + (this.state.collapsed ? ' collapse' : '')
+    return(
+          <nav
           style={{
-            color: `#222222`,
-            textDecoration: `none`,
+            background: `white`,
+            marginBottom: `1.45rem`,
           }}
         >
-        Testimonials
-        </Link>
-      </li>
-      <li style={{ 
-          margin: 0,
-          listStyle: `none`,
-          fontWeight: `bold`,
-         }}>
-        <Link
-          to="/contact"
-          style={{
-            color: `#222222`,
-            textDecoration: `none`,
-          }}
-        >
-        Contact
-        </Link>
-      </li>
-    </ul>
-  </nav>
-)
+          <ul
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `1.45rem 1.0875rem`,
+              fontFamily: `B612 Mono, monospace`,
+              fontSize: `0.6875rem`,
+            }}
+          >
+            <li className="nav-has-children" style={{ 
+                  margin: `0 0 1.32rem`, 
+                  listStyle: `none`,
+                  color: `#222222`,
+                  fontWeight: `bold`,
+              
+              }}>
+              Portfolios
+      
+              <ul className="list-unstyled">
+                  <li>Screen</li>
+                  <li className="menu-has-children">
+                    <button className="link-style-btn" onClick={this.showMenu}>
+                      Theatres
+                    </button>
+
+                      <div className={classDropdownMenu}>
+                        <ul className="dropdown">
+                          <li>Production Stills</li>
+                          <li>Rehearsals</li>
+                          <li>Press</li>
+                          <li>Headshots</li>
+                        </ul>                      
+                      </div>
+
+
+
+                    
+                  
+                  </li>
+                  <li>Commercial</li>
+                  <li>Music</li>
+                  <li>Street</li>
+                  <li>Events</li>
+                  <li>Projects</li>
+                  <li>Clients</li>
+              </ul>
+            </li>
+            <li style={{ 
+                margin: 0,
+                listStyle: `none`,
+                margin: `0 0 1.32rem`,
+                fontWeight: `bold`,
+              }}>
+             
+              Testimonials
+
+            </li>
+            <li style={{ 
+                margin: 0,
+                listStyle: `none`,
+                fontWeight: `bold`,
+              }}>
+              <a href="/contact" class="link">Contact</a>
+              
+              
+            </li>
+          </ul>
+        </nav>
+    )
+  }
+} 
+
+
 
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,14 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+
 import "../components/card.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Selected Works</h1>
-    <div style={{display: `flex`}}>
-      <figure className="card">
-        <h2 style={{textAlign: `center`}}>Overview / Title of Work</h2>  
-        <figcaption>
-          <p>In here I would write an overview of the project. A few lines about the job or something I dunno.</p>
-        </figcaption>
-      </figure>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, marginLeft: `0.45rem` }}>
-        <Image />
-      </div>
 
-    </div>
 
   </Layout>
 )
